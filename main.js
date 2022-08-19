73b737c4bcf1750fcd387e7e70314ff9253dd064
InterLink Intern <intern@intern-pc.interlink.office>
2022-08-19 18:12:42 +0300

    fix bug, create style

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-const head = document.querySelector (".header__menu");
 document.querySelector(".header__burger").onclick = () => {
     document.querySelector(".header__menu").classList.toggle("show");
     document.querySelector(".header__burger").classList.toggle("show");
     document.querySelector("body").classList.toggle("lock");
   };
+
+document.querySelector(".footer_arrow").onclick = () => {
+  document.querySelector(".footer_arrow").classList.toggle("show");
+  document.querySelector(".footer_info_body").classList.toggle("show");
+}
+// __________
+
+window.onscroll = function() {scrollFunction()};
+
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
